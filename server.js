74deb62b2344bed80bff2cbac759aaaab8efdafe
Jake Pinos <pinosjpp@gmail.com>
2024-02-04 13:09:43 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 // Budget API
 
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const port = 3000;
+
+// app.use(cors());
+
+// const budget = {
+//     myBudget: [
+//         {
+//             title: 'Eat out',
+//             budget: 25
+//         },
+//         {
+//             title: 'Rent',
+//             budget: 275
+//         },
+//         {
+//             title: 'Grocery',
+//             budget: 110
+//         },
+//     ]
+// };
+
+
+// app.get('/budget', (req, res) => {
+//     res.json(budget);
+// });
+
+// app.listen(port, () => {
+//     console.log(`API served at http://localhost:${port}`);
+// });
+
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
@@ -24,11 +56,14 @@ const budget = {
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`Example app liostening at http://localhost:${port}`);
 });
