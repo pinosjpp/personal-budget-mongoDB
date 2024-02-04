ff04e6658d73500e0cc8b67c87b7b03cda0ce7d1
Jake Pinos <pinosjpp@gmail.com>
2024-02-04 13:51:56 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -34,34 +34,23 @@
 // });
 
 const express = require('express');
+const fs = require('fs'); 
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+let budgetData = {};
+
+const rawData = fs.readFileSync('budget_data.json');
+budgetData = JSON.parse(rawData);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
